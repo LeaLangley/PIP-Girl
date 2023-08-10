f985968b1de40ccf7532138249fafe633151245c
LeaLangley <127053138+LeaLangley@users.noreply.github.com>
2023-08-10 10:54:11 +0200

    Update 1 PIP Girl.lua

--- 1 PIP Girl.lua
+++ 1 PIP Girl.lua
@@ -991,6 +991,22 @@ menu.toggle(PIP_Girl, "Carry Pickups", {"carrypickup"}, "Carry all Pickups on Yo
         notify("Droped "..counter.." Pickups.")
         carryingPickups = {}
     end
+end, function()
+    local counter = 0
+    local playerPed = PLAYER.PLAYER_PED_ID()
+    local pos = players.get_position(players.user())
+    for _, pickup in ipairs(carryingPickups) do
+        requestControl(pickup, 0)
+        util.yield(13)
+        ENTITY.DETACH_ENTITY(pickup, true, true)
+        util.yield(13)
+        ENTITY.SET_ENTITY_COORDS(pickup, pos.x, pos.y, pos.z-0.8, false, false, false, false)
+        util.yield(13)
+        ENTITY.FREEZE_ENTITY_POSITION(pickup, false)
+        counter = counter + 1
+    end
+    notify("Droped "..counter.." Pickups.")
+    carryingPickups = {}
 end)
 
 menu.toggle_loop(PIP_Girl, "Pickup Shower", {}, "Take a Shower in all exsisting Pickups.", function()
@@ -2096,6 +2112,9 @@ menu.toggle_loop(Session, "Clear Traffic", {"antitrafic"}, "Clears the traffic a
         ClearTraficSphere = 0
         util.yield(13666)
     end
+end, function()
+    MISC.REMOVE_POP_MULTIPLIER_SPHERE(ClearTraficSphere, false);
+    ClearTraficSphere = 0
 end)
 
 menu.toggle_loop(Session, "Smart Script Host", {"pgssh"}, "A Smart Script host that will help YOU if stuck in loading screens etc.", function()
