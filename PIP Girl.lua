f274cb8a2bc7adc2e8c9254477d72e6a055435c8
LeaLangley <127053138+LeaLangley@users.noreply.github.com>
2023-12-14 11:45:42 +0100

    Update PIP Girl.lua

--- PIP Girl.lua
+++ PIP Girl.lua
@@ -76,7 +76,8 @@ for _, dependency in pairs(auto_update_config.dependencies) do
     end
 end
 
-util.require_natives(1663599433)
+util.require_natives(1681379138)
+--util.require_natives("2944b", "g")
 
 resources_dir = filesystem.resources_dir() .. '/1 PIP Girl/'
 logo = directx.create_texture(resources_dir .. 'logo.png')
