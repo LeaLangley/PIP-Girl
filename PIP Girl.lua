4c32da5375858e2cc103e41d9f9cc48e0d76b5e6
LeaLangley <127053138+LeaLangley@users.noreply.github.com>
2023-12-18 19:52:58 +0100

    ~

--- PIP Girl.lua
+++ PIP Girl.lua
@@ -2292,6 +2292,7 @@ local warningMessages = {
     [446584149] = "The session you're trying to join is currently full of players. Joined queue.",
     [1691856278] = "The session you are trying to join is private and friends only. You must be invited by a friend to join this session. Joining a new GTA Online session.",
     [1910579138] = "The session you are trying to join is private and friends only. You must be invited by a friend to join this session. Joining a new GTA Online session.",
+    [163296038] = "The session you are trying to join is private and friends only. You must be invited by a friend to join this session. Joining a new GTA Online session.",
     [1073504880] = "There is a slot available in the GTA Online session you are currently queuing for. Joining Session.",
     [1748022689] = "Failed to join intended GTA Online session. Please return to Grand Theft Auto V and try again later."
 }
