76d4e034a5c1b1327be134953280494254692a38
LeaLangley <127053138+LeaLangley@users.noreply.github.com>
2023-12-20 22:33:59 +0100

    ~

--- PIP Girl.lua
+++ PIP Girl.lua
@@ -2721,8 +2721,8 @@ menu.toggle_loop(Session, "Session Claimer", {"claimsession"}, "Finds a Session 
                     --  Is session under controll?
                     --  <3
                     if PLAYER.GET_NUMBER_OF_PLAYERS() ~= 1 and (players.get_host() == players.user() or isHostFriendly) then
-                        local host_remove_block = "Online>Session>Block Joins>Removed Players>"..host_name
-                        menu.trigger_command(menu.ref_by_path(host_remove_block))
+                        --local host_remove_block = "Online>Session>Block Joins>Removed Players>"..host_name
+                        --menu.trigger_command(menu.ref_by_path(host_remove_block))
                         warnify("Found u a new Home <3")
                         menu.trigger_commands("claimsession off")
                         if players.user() != players.get_script_host() then
