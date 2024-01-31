5ebc17c8e96e071cc6cb661867e701b1fdf8b79d
LeaLangley <127053138+LeaLangley@users.noreply.github.com>
2024-02-01 00:14:30 +0100

    ~

--- PIP Girl.lua
+++ PIP Girl.lua
@@ -3676,13 +3676,35 @@ menu.action(SessionMisc, "Notify Highest K/D", {"notifykd"}, "Notify's u with th
     ReportSessionKD(numPlayers)
 end)
 
-local pop_multiplier_id = nil
-local fix_multiplier_id = nil
-menu.toggle_loop(Session, "Clear Traffic", {"antitraffic"}, "Clears the traffic on the session for everyone.", function()
-    if fix_multiplier_id then
-        if MISC.DOES_POP_MULTIPLIER_AREA_EXIST(fix_multiplier_id) then
-            MISC.REMOVE_POP_MULTIPLIER_AREA(fix_multiplier_id, false)
+local pop_multiplier_clear = false
+local pop_multiplier_fix = false
+
+local function remove_pop_multiplier(id)
+    if id then
+        if MISC.DOES_POP_MULTIPLIER_AREA_EXIST(id) then
+            MISC.REMOVE_POP_MULTIPLIER_AREA(id, true)
         end
+    else
+        for sphere = 0, 15 do
+            if MISC.DOES_POP_MULTIPLIER_AREA_EXIST(sphere) then
+                MISC.REMOVE_POP_MULTIPLIER_AREA(sphere, true)
+            end
+        end
+    end
+end
+
+local function create_pop_multiplier(pedPOP, vehPOP)
+    for sphere = 0, 15 do
+        if not MISC.DOES_POP_MULTIPLIER_SPHERE_EXIST(sphere) then
+            MISC.ADD_POP_MULTIPLIER_SPHERE(0.0, 0.0, 0.0, 19666, pedPOP, vehPOP, false, true)
+        end
+    end
+end
+
+menu.toggle_loop(Session, "Clear Traffic", {"antitraffic"}, "Clears the traffic on the session for everyone.", function()
+    if pop_multiplier_fix then
+        remove_pop_multiplier()
+        pop_multiplier_fix = false
     end
     if menu.get_state(menu.ref_by_path("World>Inhabitants>Traffic>Disable")) == "Disabled" then
         menu.trigger_command(menu.ref_by_path("World>Inhabitants>Traffic>Disable>Enabled, Including Parked Cars"))
@@ -3691,32 +3713,33 @@ menu.toggle_loop(Session, "Clear Traffic", {"antitraffic"}, "Clears the traffic 
         menu.set_state(menu.ref_by_path("World>Inhabitants>Pedestrians>Disable"), "On")
     end
     if transitionState(true) == 1 then
-        if menu.get_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas")) == "On" then
-            menu.set_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas"), "Off")
-        end
-        if not pop_multiplier_id then
-            pop_multiplier_id = MISC.ADD_POP_MULTIPLIER_AREA(-3999.0, 7999.0, 199.0, 4499.0, -3999.0, -199.0, 0.0, 0.0, false, true)
+        if not pop_multiplier_clear then
+            pop_multiplier_clear = true
             MISC.CLEAR_AREA(0.0, 0.0, 0.0, 19999.9, true, false, false, true)
             VEHICLE.SET_DISTANT_CARS_ENABLED(false)
             STREAMING.SET_PED_POPULATION_BUDGET(0)
             STREAMING.SET_VEHICLE_POPULATION_BUDGET(0)
             STREAMING.SET_REDUCE_PED_MODEL_BUDGET(true)
             STREAMING.SET_REDUCE_VEHICLE_MODEL_BUDGET(true)
-        else
-            if not MISC.DOES_POP_MULTIPLIER_AREA_EXIST(pop_multiplier_id) then
-                pop_multiplier_id = MISC.ADD_POP_MULTIPLIER_AREA(-3999.0, 7999.0, 199.0, 4499.0, -3999.0, -199.0, 0.0, 0.0, false, true)
+            remove_pop_multiplier()
+            if menu.get_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas")) == "On" then
+                menu.set_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas"), "Off")
             end
+            create_pop_multiplier(0.0, 0.0)
+        else
+            if menu.get_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas")) == "On" then
+                menu.set_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas"), "Off")
+            end
+            create_pop_multiplier(0.0, 0.0)
             util.yield(6666)
         end
     else
         if menu.get_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas")) == "Off" then
             menu.set_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas"), "On")
         end
-        if pop_multiplier_id then
-            if MISC.DOES_POP_MULTIPLIER_AREA_EXIST(pop_multiplier_id) then
-                MISC.REMOVE_POP_MULTIPLIER_AREA(pop_multiplier_id, false)
-            end
-            pop_multiplier_id = nil
+        if pop_multiplier_clear then
+            remove_pop_multiplier()
+            pop_multiplier_clear = false
         else
             util.yield(6666)
         end
@@ -3736,16 +3759,13 @@ end, function()
     STREAMING.SET_VEHICLE_POPULATION_BUDGET(3)
     STREAMING.SET_REDUCE_PED_MODEL_BUDGET(false)
     STREAMING.SET_REDUCE_VEHICLE_MODEL_BUDGET(false)
-    if pop_multiplier_id then
-        if MISC.DOES_POP_MULTIPLIER_AREA_EXIST(pop_multiplier_id) then
-            MISC.REMOVE_POP_MULTIPLIER_AREA(pop_multiplier_id, false)
-        end
-    end
-    pop_multiplier_id = nil
+    pop_multiplier_clear = false
     if menu.get_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas")) == "On" then
         menu.set_state(menu.ref_by_path("Online>Protections>Delete Modded Pop Multiplier Areas"), "Off")
     end
-    fix_multiplier_id = MISC.ADD_POP_MULTIPLIER_AREA(-3999.0, 7999.0, 199.0, 4499.0, -3999.0, -199.0, 0.13, 0.13, false, true)
+    remove_pop_multiplier()
+    create_pop_multiplier(1.0, 0.25)
+    pop_multiplier_fix = true
 end)
 
 menu.toggle_loop(Session, "Soft Clear Traffic", {"softantitrafic"}, "Clears the traffic around you localy in close range.\nDosnt work with many players in close range.", function()
@@ -4380,7 +4400,16 @@ menu.action(menu.my_root(), "Activate Everyday Goodies", {"pggoodies"}, "Activat
 end)
 
 menu.action(menu.my_root(), "Update Notes", {""}, startupmsg, function()
-    notify(startupmsg)
+    --notify(startupmsg)
+    if pop_multiplier_id then
+        notify(pop_multiplier_id)
+    end
+    if MISC.DOES_POP_MULTIPLIER_AREA_EXIST(pop_multiplier_id) then
+        notify("beep")
+    end
+    if MISC.IS_POP_MULTIPLIER_AREA_NETWORKED(pop_multiplier_id) then
+        notify("yes")
+    end
 end)
 
 menu.trigger_commands("antiadmin")
