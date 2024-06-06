990e5d2644772b25f9276046bb73d03e2b319da4
LeaLangley <127053138+LeaLangley@users.noreply.github.com>
2024-06-06 18:36:06 +0200

    <- Redirect for old version user.

--- PIP Girl.lua
+++ PIP Girl.lua
@@ -12,34 +12,6 @@ local auto_update_config = {
     source_url="https://raw.githubusercontent.com/LeaLangley/PIP-Girl/main/PIP%20Girl.pluto",
     script_relpath=SCRIPT_RELPATH,
     verify_file_begins_with="--",
-    --auto_restart=false,
     check_interval=0,
-    restart_delay=666,
-    dependencies={
-        {
-            name="Asuka_Libiary",
-            source_url="https://raw.githubusercontent.com/LeaLangley/Asuka_Library/main/Asuka_Lib.pluto",
-            script_relpath="lib/Asuka_Library/Asuka_Lib.pluto",
-            check_interval=0,
-        },
-        {
-            name="logo",
-            source_url="https://raw.githubusercontent.com/LeaLangley/PIP-Girl/main/resources/PIP-Girl/logo.png",
-            script_relpath="resources/PIP Girl/logo.png",
-            check_interval=13666,
-        },
-        {
-            name="blacklist",
-            source_url="https://raw.githubusercontent.com/LeaLangley/PIP-Girl/main/resources/PIP-Girl/Blacklist.json",
-            script_relpath="resources/PIP Girl/Blacklist.json",
-            check_interval=0,
-        },
-        {
-            name="read_me.txt",
-            source_url="https://raw.githubusercontent.com/LeaLangley/PIP-Girl/main/resources/PIP-Girl/Export/read_me.txt",
-            script_relpath="resources/PIP Girl/Export/read_me.txt",
-            check_interval=13666,
-        },
-    }
 }
 auto_updater.run_auto_update(auto_update_config)
