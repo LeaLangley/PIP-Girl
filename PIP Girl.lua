e16ca1a6c3842ecadc2640150b68e0892ecbd5b5
LeaLangley <127053138+LeaLangley@users.noreply.github.com>
2024-06-06 18:45:43 +0200

    <- Redirect for old version user.

--- PIP Girl.lua
+++ PIP Girl.lua
@@ -1,15 +1,15 @@
 --[[
-__________._____________    ________.__       .__   
-\______   \   \______   \  /  _____/|__|______|  |  
- |     ___/   ||     ___/ /   \  ___|  \_  __ \  |  
+__________._____________    ________.__       .__
+\______   \   \______   \  /  _____/|__|______|  |
+ |     ___/   ||     ___/ /   \  ___|  \_  __ \  |
  |    |   |   ||    |     \    \_\  \  ||  | \/  |__
- |____|   |___||____|      \________/__||__|  |____/                
+ |____|   |___||____|      \________/__||__|  |____/
 ]]--
 util.ensure_package_is_installed("lua/auto-updater")
 local auto_updater = require("auto-updater")
 if not async_http.have_access() then util.toast("U need to allow internet access to download all resources the script needs to work and receive cutting edge updates. <3"); return end
 local auto_update_config = {
-    source_url="https://raw.githubusercontent.com/LeaLangley/PIP-Girl/main/PIP%20Girl.pluto",
+    source_url="https://raw.githubusercontent.com/LeaLangley/PIP-Girl/main/PIP-Girl.pluto",
     script_relpath=SCRIPT_RELPATH,
     verify_file_begins_with="--",
     check_interval=0,
